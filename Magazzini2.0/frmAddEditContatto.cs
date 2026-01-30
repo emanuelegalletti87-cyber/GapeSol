cc655b3faf7e7dd45f963654dbbbb30528c94f8d
egalletti <e.galletti@isolutions.it>
2026-01-30 10:02:45 +0100

    first commit

--- Magazzini2.0/frmAddEditContatto.cs
+++ Magazzini2.0/frmAddEditContatto.cs
@@ -45,6 +45,7 @@ namespace Magazzini2._0
             Close();
         }
 
+
         private bool ValidaCampi()
         {
             if (string.IsNullOrEmpty(txtbNome.Text) || string.IsNullOrEmpty(txtbCognome.Text) || string.IsNullOrEmpty(txtbIndirizzo.Text) || string.IsNullOrEmpty(txtbCitta.Text) || string.IsNullOrEmpty(txtbEmail.Text) || string.IsNullOrEmpty(txtbTelefono.Text))
